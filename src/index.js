36d49d789d5a8cb0a618fc80652bd339fa454065
Adarsh singh <adarshsingh0930@gmail.com>
2024-07-29 20:59:39 +0530

    adds tasks router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const tasksRouter = require("./routes");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/", (req, res) => res.send());
+app.use("/tasks", tasksRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
