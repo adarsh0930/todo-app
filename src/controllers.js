c9399f11f481bfe8c7fbd32e0f704df3d324d76e
Adarsh singh <adarshsingh0930@gmail.com>
2024-07-29 20:51:17 +0530

    add routes with empty request handlers

---
+++ src/controllers.js
@@ -0,0 +1,27 @@
+async function getTaskById(req, res) {
+  res.send();
+}
+
+async function getTasks(req, res) {
+  res.send();
+}
+
+async function createTask(req, res) {
+  res.send();
+}
+
+async function updateTask(req, res) {
+  res.send();
+}
+
+async function deleteTask(req, res) {
+  res.send();
+}
+
+module.exports = {
+  createTask,
+  getTaskById,
+  getTasks,
+  updateTask,
+  deleteTask,
+};
