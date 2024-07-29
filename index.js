1550fedd546457be7b52c5ca552cbedb69302c4b
Adarsh singh <adarshsingh0930@gmail.com>
2024-07-29 20:42:30 +0530

    adds an express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("To do App!");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/", (req, res) => res.send());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
