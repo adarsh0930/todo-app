be56e6b204d2d3477fcd721867893e7264a865b2
Adarsh singh <adarshsingh0930@gmail.com>
2024-07-29 20:59:56 +0530

    adds error logs

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const fs = require("node:fs/promises");
 const path = require("node:path");
 
-const tasksFilePath = path.join(__dirname, "../../tasks.json");
+const tasksFilePath = path.join(__dirname, "../tasks.json");
 
 // Read all tasks from a JSON file
 async function readTasks() {
@@ -9,13 +9,18 @@ async function readTasks() {
     const data = await fs.readFile(tasksFilePath, "utf8");
     return JSON.parse(data);
   } catch (err) {
+    console.error(err);
     return [];
   }
 }
 
 // Write the list of tasks to a JSON file
 async function writeTasks(tasks) {
-  await fs.writeFile(tasksFilePath, JSON.stringify(tasks, null, 2));
+  try {
+    await fs.writeFile(tasksFilePath, JSON.stringify(tasks, null, 2));
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 module.exports = { readTasks, writeTasks };
