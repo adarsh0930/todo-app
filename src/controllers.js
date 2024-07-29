21f838a496b9bd42c0cf72790b43ae7e9c3ce713
Adarsh singh <adarshsingh0930@gmail.com>
2024-07-29 21:00:25 +0530

    adds controllers for get APIs

--- src/controllers.js
+++ src/controllers.js
@@ -1,9 +1,20 @@
+const { readTasks, writeTasks } = require("./db");
+
 async function getTaskById(req, res) {
-  res.send();
+  const tasks = await readTasks();
+  const currentTasks = tasks.filter((task) => task.isDeleted == false);
+  const task = currentTasks.find((task) => task.id == req.params.id);
+  if (!task) {
+    return res.status(404).json({ message: "Task not found" });
+  }
+
+  res.json(task);
 }
 
 async function getTasks(req, res) {
-  res.send();
+  const tasks = await readTasks();
+  const currentTasks = tasks.filter((task) => task.isDeleted == false);
+  res.json(currentTasks);
 }
 
 async function createTask(req, res) {
